cf319d3716fe4b248a740e380bd42d3e36e9ae2c
Malcolm Mitchell <mitchellm@carleton.edu>
2018-07-31 18:34:58 -0500

    Nearly complete stoplist, output section + much documentation

--- Stopifu.py
+++ Stopifu.py
@@ -1,79 +1,103 @@
 import os
+from datetime import date
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def stopifu():
-    return render_template('index.html')
+# Returns names of all stoplist files in stoplist directory
+def getStoplistNames():
+    stoplistNames = []
+    for item in os.listdir('stoplists/'):
+        stoplistNames.append(item.strip(".txt"))
+    return stoplistNames
 
-@app.route('/get_stoplist/<name>')
-def getStoplist(name):
+# Gets stoplist information
+@app.route('/get_stoplist/<name>/')
+def getStoplist(name, jsonifyCheck = True):
     stoplist = {}
     stopwords = []
     stoplist['stopwords'] = stopwords
     if os.path.isfile("stoplists/" + name + ".txt"):
         with open("stoplists/" + name + ".txt", "r+") as file:
-            metadata = file.readline().split()
-            stopwords = file.readline().split()
+            metadata = file.readline().split("\t")
+            for line in file:
+                stoplist['stopwords'].append(line.strip())
             stoplist['name'] = metadata[0]
             stoplist['category'] = metadata[1]
             stoplist['date'] = metadata[2]
-            for word in stopwords:
-                stoplist['stopwords'].append(word)
-        return stoplist
+        if jsonifyCheck:
+            return jsonify({ 'stoplist':stoplist })
+        else:
+            return stoplist
     else:
         return "No such file exists."
+
+# Gets all stoplist
+@app.route('/get_all_stoplists/')
+def getAllStoplists():
+    allStoplists = []
+    stoplistNames = getStoplistNames()
+    for stoplistName in stoplistNames:
+        allStoplists.append(getStoplist(stoplistName, False))
+    return jsonify({  'allStoplists':allStoplists })
     
-@app.route('/add_stoplist')
-def addStoplist(name, category, date, stopwords):
+# Creates a new stoplist (if valid name)
+@app.route('/create_stoplist/<name>/<category>/<date>')
+def createStoplist(name, category, date):
     if not os.path.isfile("stoplists/" + name + ".txt"):
-        with open("stoplists/" + name + ".txt", "w+") as file:
-            file.write(name + " " + category + " " + date + "\n")
-            file.write(" ".join(stopwords))
-            file.close()
+        with open("stoplists/" + name + ".txt", "w") as file:
+            file.write(name + "\t" + category + "\t" + date + "\n")
         return jsonify({})
     else:
         return "A stoplist with this name already exists"
 
-#  (Repurposed from Kipp's Stopify)
+    
+# Removes a stoplist
+@app.route('/remove_stoplist/<name>')
+def removeStoplist(name):
+    if os.path.isfile("stoplists/" + name + ".txt"):
+        os.remove("stoplists/" + name + ".txt")
+        return jsonify({})
+    else: 
+        return "No such file exists."
+
+# Adds a word to a stoplist file (repurposed from Kipp's Stopify)
 @app.route('/add_to_stoplist/<name>/<word>')
 def addToStoplist(name, word):
     if os.path.isfile("stoplists/" + name + ".txt"):
         with open("stoplists/" + name + ".txt", "a+") as file:
-            file.write("\n%s" % word)
+            file.write("%s\n" % word)
             file.close();
         return jsonify({})
     else:
         return "No such file exists."
         
-# TODO Find way to replace text without writing new file. This doens't work. (Repurposed from Kipp's Stopify)
+# TODO Make finding word easier (try to include what position a word is in stoplist in its object)
+# Removes a word from a stoplist file
 @app.route('/remove_from_stoplist/<name>/<word>')
 def removeFromStoplist(name, word):
     if os.path.isfile("stoplists/" + name + ".txt"):
-        with open("stoplists/" + name + ".txt", "r+") as file:
-            firstLine = True
+        # Copies every line from stoplist file, except word-to-remove
+        stoplist = []
+        with open("stoplists/" + name + ".txt", 'r') as file:
             for line in file:
-                if firstLine:
-                    firstLine = False
-                else:
-                    if word in line:
-                        line.replace(word, "")
-                        return line
-            file.close();
+                if not (line.strip() == word):
+                    stoplist.append(line.strip() + "\n")
+        # Replaces stoplist file with copy
+        with open("stoplists/" + name + ".txt", 'w') as file:
+            file.writelines(stoplist)
         return jsonify({})
     else:
         return "No such file exists."
-    
-# Returns active documents
-@app.route('/get_document_list')
-def getDocumentList(path):
+
+# Returns names of all active documents
+def getDocumentNames(path):
     docs = []
     for item in os.listdir(path):
-        docs.append(item)
+        docs.append(item.strip(".txt"))
     return docs
 
-# Return the contents of a document (Repurposed from Kipp's Stopify)
+# Gets jsonified document information (Repurposed from Kipp's Stopify)
 @app.route('/get_document/<name>')
 def getDocument(path, name):
     contents = []
@@ -82,9 +106,16 @@ def getDocument(path, name):
             for line in file:
                 contents.append(line.strip())
         contents = ''.join(contents)
-        return contents
+        return jsonify({ 'contents':contents })
     else:
         return "No such file exists."
 
+# Render index.html
+@app.route('/')
+def home():
+    return render_template('index.html',
+                            documentNames = getDocumentNames("doc/"),
+                            stoplistNames = getStoplistNames())
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
---
+++ static/js/home.js
@@ -0,0 +1,278 @@
+var stoplistList = [];
+var stoplistIndices = {};
+var sessionStopwords = [];
+var stopwordIndices = {};
+
+// Returns current date
+function getDate() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    return mm + '-' + dd + '-' + yyyy;
+}
+
+// Reveals addStoplistPopup and creates stoplist from input (adds text file and to stoplistList)
+function createStoplist() {
+    d3.select("#confirmStoplist-btn").on("click", function() {
+        var name = d3.select("#createStoplistName").property("value").trim();
+        var category = d3.select("#createStoplistCategory").property("value").trim();
+        var stopwords = d3.select("#createStoplistWords").property("value").trim().split(" ");
+        var date = getDate();
+        
+        if (name != "" && category != "") {
+            var stopURL = '/create_stoplist/' + name + '/' + category + '/' + date;
+            d3.json(stopURL, function(foo) { });
+
+            for (var i = 0; i < stopwords.length; i++) {
+                var stopURL = '/add_to_stoplist/' + name + '/' + stopwords[i];
+                d3.json(stopURL, function(foo) { });
+            }
+            $('#createStoplistName').val('');
+            $('#createStoplistCategory').val('');
+            $('#createStoplistWords').val('');
+            $('#addStoplistPopup').modal('hide');
+            stoplistIndices[name] = stoplistList.length;
+            stoplistList.push({
+                name: name,
+                category: category,
+                date: date,
+                stopwords: stopwords,
+                active: false
+            });
+            updateStoplistVisuals();
+        }
+    });
+    $('#addStoplistPopup').modal('show');
+}
+
+// Removes a stopword (from backend and stoplistList)
+function removeStopword(name, word) {
+    var stopURL = '/remove_from_stoplist/' + name + '/' + word;
+    d3.json(stopURL, function(foo) { });
+    // Updates stoplist from file
+    var stopURL = '/get_stoplist/' + name;
+    d3.json(stopURL, function(foo) { 
+        stoplistList[stoplistIndices[name]].stopwords = foo['stoplist']['stopwords'];
+    });
+
+//    // This uses stoplistList as data instead of backend. (seems to cause issues) 
+//    stopwords = stoplistList[stoplistIndices[name]].stopwords;
+//    for (i = 0; i < stopwords.length; i++) {
+//        if (stopwords[i] == word) {
+//            stopwords.splice(i, 1);
+//        }
+//    }
+    
+}
+
+// Updates stoplistEdit display to include all stopwords (from stoplist file)
+function updateStoplistEditVisuals(name) {
+    var stopURL = '/get_stoplist/' + name + '/';
+    d3.json(stopURL, function(foo) {
+        var stopwordElem = d3.select('#stoplistEditWords').selectAll('.stopword')
+            .data(foo['stoplist']['stopwords'], function(d) { return d; });
+        
+        stopwordElem.enter()
+            .append("span")
+            .attr("class", "stopword stopword-" + name)
+            .text(function(d) { return d; })
+            .append("button")
+            .attr("class", "close stopwordRemove-btn")
+            .append("span")
+            .html("&times;")
+            .on("click", function (d) {
+                removeStopword(name, d);
+                updateStoplistEditVisuals(name);
+            });
+        
+        stopwordElem.exit().remove();
+    });
+}
+
+// Updates output section to include active stopwords (from sessionStopwords)
+function updateOutputVisuals() {
+    var stopwordElem = d3.select('#sub_fullStoplist').selectAll('.stopword')
+        .data((sessionStopwords.filter(function(d){ return d.inclusions > 0 && d.enabled === true; })), function(d) {
+            return d.word;
+        });
+
+    stopwordElem.enter()
+        .append("span")
+        .attr("class", "stopword stopword-" + function(d) { return d.word; })
+        .text(function(d) { return d.word; })
+        .append("button")
+        .attr("class", "close stopwordRemove-btn")
+        .append("span")
+        .html("&times;")
+        .on("click", function(d) {
+            d.enabled = false;
+            updateOutputVisuals();
+        })
+
+    stopwordElem.exit().remove();
+}
+
+// Deletes a stoplist (from stoplist file and stoplistList)
+function deleteStoplist(name) {
+    if (confirm("Are you certain?")) {
+        var stopURL = '/remove_stoplist/' + name;
+        d3.json(stopURL, function(foo) {
+            d3.select("#stoplist-" + name).remove();
+        });
+        stoplistList.splice(stoplistIndices[name], 1);
+        delete stoplistIndices[name];
+        updateStoplistVisuals();
+    }
+}
+
+// Reveals and updates the editStoplistPopup to the selected stoplist
+function generateStoplistEditDisplay(name) {
+    d3.select("#stoplistEditTitle").text(name);
+    d3.select("#stoplistEditWords").selectAll("span").remove();
+    $('#stoplistEditform').val('');
+    d3.select("#stoplistAddBtn").on("click", function() {
+        words = d3.select("#stoplistEditform").property("value").split(" ");
+        for (var i = 0; i < words.length; i++) {
+            // Pushes valid words to stoplistList and stoplist file
+            word = words[i].trim();
+            if (word != "") {
+                var stopURL = '/add_to_stoplist/' + name + '/' + word;
+                d3.json(stopURL, function(foo) { });
+                stoplistList[stoplistIndices[name]].stopwords.push(word);
+            }
+        }
+        updateStoplistEditVisuals(name);
+        $('#stoplistEditform').val('');
+    });
+    updateStoplistEditVisuals(name);
+
+    
+    $('#editStoplistPopup').modal('show');
+}
+
+// Toggles the activation of a stoplist, updating sessionStopwords (uses stoplistList as data)
+function toggleStoplistActivation(stoplistElem, name) {
+    var stoplist = stoplistList[stoplistIndices[name]];
+    var stopwords = stoplist.stopwords;
+    // Activates, if deactivated
+    if (!stoplist.active) {
+        // Adds active html
+        d3.select(stoplistElem).style("outline", "solid 2px rgba(0,255,0,.8)");
+        // Activates stopwords
+        for (i = 0; i < stopwords.length; i++) {
+            var word = stopwords[i];
+            if (!(word in stopwordIndices)) {
+                stopwordIndices[word] = sessionStopwords.length;
+                sessionStopwords.push({
+                    word: word,
+                    inclusions: 1,
+                    enabled: true
+                });
+            }
+            else {
+               sessionStopwords[stopwordIndices[word]].inclusions += 1;
+            }
+        }
+    }
+    // Deactivates, if activated
+    else {
+        // Clear html
+        d3.select(stoplistElem).style("outline", "none");
+        // Deactivates stopwords
+        for (i = 0; i < stopwords.length; i++) {
+            sessionStopwords[stopwordIndices[stopwords[i]]].inclusions -= 1;
+        }
+    }
+    stoplist.active = !stoplist.active;
+    updateOutputVisuals();
+}
+
+// Generates HTML and CSS for a stoplist element (uses only params as data)
+function generateStoplistHTML(stoplistElem, name, category, date, stopwords) {
+    // Allows hover for stoplist element
+    d3.select(stoplistElem)
+        .on('mouseover', function() { $(stoplistElem).children(".btn").show(); })
+        .on('mouseout', function() { $(stoplistElem).children(".btn").hide(); })
+    // Appends stoplistInfoWrapper containing metadata and stopword display
+    var stoplistInfoWrapper = d3.select(stoplistElem)
+        .append("div")
+        .attr("class", "stoplistInfoWrapper-" + name)
+        .on("click" , function() {
+            toggleStoplistActivation(stoplistElem, name);
+        })
+        .append("div")
+            .attr("class", "p-1 stop-metadata")
+            .text(function() {
+                if (name.length + category.length + date.length > 30) { return name.substring(0, 15) + "..."; } 
+                else { return name + "\t" + category + "\t" + date; }
+            });
+    var stopwordPreview = d3.select(".stoplistInfoWrapper-" + name)
+        .append("div")
+            .attr("class", "p-1 stopword-preview")
+            .text(function() {
+                if (stopwords.length > 8) { return stopwords.slice(0, 8).join(", ") + "..."; } 
+                else { return stopwords.slice(0, stopwords.length).join(", "); }
+            })
+    // Appends (hover-only) edit and delete buttons
+    var editBtn = d3.select(stoplistElem)
+        .append("button")
+        .attr("class", "btn stoplistEdit-btn")
+        .text("edit")
+        .on("click", function() {
+            if (stoplistList[stoplistIndices[name]].active) { toggleStoplistActivation(stoplistElem, name); }
+            generateStoplistEditDisplay(name);
+        })
+    var deleteBtn = d3.select(stoplistElem)
+        .append("button")
+        .attr("class", "btn btn-danger stoplistDelete-btn")
+        .text("delete")
+        .on("click", function() {
+            if (stoplistList[stoplistIndices[name]].active) { toggleStoplistActivation(stoplistElem, name); }
+            deleteStoplist(name);
+        });
+    $(stoplistElem).children(".btn").hide();
+}
+
+// Updates HTML elements for stoplists for stoplistList data 
+function updateStoplistVisuals() {
+    var stoplistElem = d3.select("#stoplists").selectAll('.stoplist')
+        .data(stoplistList, function(d) {
+            return d.name;
+        })
+    
+    stoplistElem.enter()
+        .append("div")
+        .attr("class", "stoplist")
+        .attr("id", "listwrapper-" + (function(d) {
+            return d.name;
+        }))
+        .each(function(d) {
+            generateStoplistHTML(this, d.name, d.category, d.date, d.stopwords);
+        });
+    
+    stoplistElem.exit().remove();
+}
+
+// Gets all current stoplists from Stopifu.py, adds each to stoplistList
+function addExistingStoplists() {
+    var stopURL = '/get_all_stoplists/'
+    d3.json(stopURL, function(foo) {
+        for (i = 0; i < foo['allStoplists'].length; i++) {
+            stoplistIndices[foo['allStoplists'][i]['name']] = stoplistList.length;
+            stoplistList.push({
+                name: foo['allStoplists'][i]['name'],
+                category: foo['allStoplists'][i]['category'],
+                date: foo['allStoplists'][i]['date'],
+                stopwords: foo['allStoplists'][i]['stopwords'],
+                active: false
+            });
+        }
+        updateStoplistVisuals();
+    });
+}
+
+// Main function
+window.onload = function() {
+    addExistingStoplists();
+}
